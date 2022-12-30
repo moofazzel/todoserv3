94373c17fc73dd8e96d19ea140597ee27ccc405d
moofazzel <2192461016@uttarauniversity.edu.bd>
2022-12-30 18:34:20 +0600

    deploy 2

--- components/AddTask.js
+++ components/AddTask.js
@@ -41,7 +41,7 @@ const AddTask = () => {
             <input
               name="task"
               type="text"
-              className="group !bg-gray-50 border-0 shadow-lg border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-3  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="!text-black group !bg-gray-50 border-0 shadow-lg border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-3  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Add Task"
             />
             <BiPlus className="text-gray-400 text-3xl absolute top-[8px] left-3" />
--- pages/my_task.js
+++ pages/my_task.js
@@ -6,53 +6,52 @@ const my_task = ({ data }) => {
   return (
     <>
       <div className="container pt-20">
-        {data.map((task) => {
-          <div className="md:w-[90%] mx-auto mb-3 ">
-            <div className="w-full inline-flex items-center bg-white leading-none rounded-xl p-2 pl-4 shadow text-teal text-sm group/edit">
-              <button className="inline-flex border border-gray-700 text-white rounded-xl h-5 w-5 hover:w-6 hover:h-6 justify-center items-center hover:transition-all hover:duration-300 group/item mr-1">
-                <BiCheckDouble className="hidden group-hover/item:block hover:text-xl text-green-500 hover:transition-all hover:duration-500" />
-              </button>
-              <span className="text-lg inline-flex px-2 text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti, dolore.
-              </span>
-              <span className="text-lg inline-flex px-2 text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti, dolore.
-              </span>
-              <span className="text-lg inline-flex px-2 text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti, dolore.
-              </span>
-              <span className="text-lg inline-flex px-2 text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti, dolore.
-              </span>
+        <div className="md:w-[90%] mx-auto mb-3 ">
+          <div className="w-full inline-flex items-center bg-white leading-none rounded-xl p-2 pl-4 shadow text-teal text-sm group/edit">
+            <button className="inline-flex border border-gray-700 text-white rounded-xl h-5 w-5 hover:w-6 hover:h-6 justify-center items-center hover:transition-all hover:duration-300 group/item mr-1">
+              <BiCheckDouble className="hidden group-hover/item:block hover:text-xl text-green-500 hover:transition-all hover:duration-500" />
+            </button>
+            <span className="text-lg inline-flex px-2 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              dolore.
+            </span>
+            <span className="text-lg inline-flex px-2 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              dolore.
+            </span>
+            <span className="text-lg inline-flex px-2 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              dolore.
+            </span>
+            <span className="text-lg inline-flex px-2 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              dolore.
+            </span>
 
-              {/* <button className="ml-auto mr-3"></button> */}
+            {/* <button className="ml-auto mr-3"></button> */}
 
-              <Dropdown>
-                <Dropdown.Button className="ml-auto"></Dropdown.Button>
+            <Dropdown>
+              <Dropdown.Button className="ml-auto"></Dropdown.Button>
 
-                <Dropdown.Menu aria-label="Actions" variant="light">
-                  <Dropdown.Item key="edit">
-                    <span className="flex items-center">
-                      <BiEditAlt /> Edit
-                    </span>
-                  </Dropdown.Item>
-                  <Dropdown.Item key="delete" color="error">
-                    <span
-                      onClick={() => deleteTask(_id)}
-                      className="flex items-center"
-                    >
-                      <AiOutlineDelete /> Delete
-                    </span>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
-          </div>;
-        })}
+              <Dropdown.Menu aria-label="Actions" variant="light">
+                <Dropdown.Item key="edit">
+                  <span className="flex items-center">
+                    <BiEditAlt /> Edit
+                  </span>
+                </Dropdown.Item>
+                <Dropdown.Item key="delete" color="error">
+                  <span
+                    onClick={() => deleteTask(_id)}
+                    className="flex items-center"
+                  >
+                    <AiOutlineDelete /> Delete
+                  </span>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        ;
       </div>
     </>
   );
